930e60973e1cf18033f649bb50fa833619b9d66f
Enoch <mmejuenoch@gmail.com>
2023-05-06 21:08:14 +0100

    Added the google font Readex pro to entire webpage, applied some custom styles to the h1 and h2 and a tags.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["index.html", "./src/**/*.ts"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        readex: ['Readex Pro']
+      }
+    },
   },
   plugins: [],
 };
