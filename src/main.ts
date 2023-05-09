1eb6747b85f1f91973a73e7ed9802fab1a9db0c2
Enoch <mmejuenoch@gmail.com>
2023-05-09 22:26:04 +0100

    Added section for work history, and styled it. Added section for recent project and styled it. Added the footer showing the copyright notice. Added the structure and styling for the themeSwitcher and the logic for it as well in the main.ts file.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,36 @@
 import './style/global.css';
+
+// 🌞 , 🌚 theme emojis for content
+
+const themeSwitch = document.querySelector("#themeSwitcher")!;
+const htmlDoc = document.documentElement;
+
+//set theme
+const localData = localStorage.getItem("theme");
+if(localData !== null) {
+  if(localData == "") {
+    htmlDoc.classList.remove("dark");
+    themeSwitch.innerHTML = `<span>🌚</span>`;
+  }else {
+    htmlDoc.classList.add("dark");
+    themeSwitch.innerHTML = `<span>🌞</span>`;
+  }
+}else {
+  htmlDoc.classList.add("dark");
+  themeSwitch.innerHTML = `<span>🌞</span>`;
+}
+
+// on click
+themeSwitch.addEventListener("click", () => {
+  if(htmlDoc.classList.contains("dark")) {
+    htmlDoc.classList.remove("dark")
+    localStorage.setItem("theme", "")
+    //set content
+    themeSwitch.innerHTML = `<span>🌚</span>`;
+  }else {
+    htmlDoc.classList.add("dark");
+    localStorage.setItem("theme", "dark")
+    //set content
+    themeSwitch.innerHTML = `<span>🌞</span>`;
+  }
+})
